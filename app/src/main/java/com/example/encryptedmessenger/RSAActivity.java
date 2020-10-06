5cacf96e6b95d464db7c930e7ac302f48c91927a
Anuraj Deep <deep.anuraj10@gmail.com>
2020-10-06 16:37:36 +0530

    RSA encryption decryption

--- app/src/main/java/com/example/encryptedmessenger/RSAActivity.java
+++ app/src/main/java/com/example/encryptedmessenger/RSAActivity.java
@@ -1,10 +1,42 @@
 package com.example.encryptedmessenger;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import javax.crypto.Cipher;
 
 public class RSAActivity extends AppCompatActivity {
+
+    EditText textInput;
+    ImageView voiceInput;
+    TextView outputText;
+    Button enc, dec, clear, send;
+    String temp;
+    String tosend = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +44,161 @@ public class RSAActivity extends AppCompatActivity {
         setContentView(R.layout.activity_r_s_a);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("RSA Encryption Decryption");
+        initViews();
+
+        KeyPair kp = getKeyPair();
+
+        PublicKey publicKey = kp.getPublic();
+        final byte[] publicKeyBytes = publicKey.getEncoded();
+        final String publicKeyBytesBase64 = new String(Base64.encode(publicKeyBytes, Base64.DEFAULT));
+
+        PrivateKey privateKey = kp.getPrivate();
+        byte[] privateKeyBytes = privateKey.getEncoded();
+        final String privateKeyBytesBase64 = new String(Base64.encode(privateKeyBytes, Base64.DEFAULT));
+
+
+        enc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                temp = textInput.getText().toString();
+                String encrypted = encryptRSAToString(temp, publicKeyBytesBase64);
+                outputText.setText(encrypted);
+                tosend = encrypted;
+
+            }
+        });
+
+        dec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                temp = textInput.getText().toString();
+                String decrypted = decryptRSAToString(temp, privateKeyBytesBase64);
+                outputText.setText(decrypted);
+                tosend = decrypted;
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    textInput.setText("");
+                    outputText.setText("");
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tosend.length()>0) {
+                    Intent sendIntent = new Intent();
+                    sendIntent.setAction(Intent.ACTION_SEND);
+                    sendIntent.putExtra(Intent.EXTRA_TEXT, tosend);
+                    sendIntent.setType("text/plain");
+                    startActivity(sendIntent);
+                } else {
+                    Toast.makeText(RSAActivity.this, "Empty Output", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        voiceInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+                if(intent.resolveActivity(getPackageManager())!=null) {
+                    startActivityForResult(intent, 1001);
+                } else {
+                    Toast.makeText(RSAActivity.this, "your device does not support this feature", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
+    public void initViews() {
+        textInput = findViewById(R.id.text_input);
+        outputText = findViewById(R.id.output_text);
+        voiceInput = findViewById(R.id.voice_input);
+        enc = findViewById(R.id.enc);
+        dec = findViewById(R.id.dec);
+        clear = findViewById(R.id.clear_button);
+        send = findViewById(R.id.send);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1001:
+                if (resultCode==RESULT_OK && data!=null){
+                    ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    textInput.setText(res.get(0));
+                }
+                break;
+        }
+    }
+
+    public static KeyPair getKeyPair() {
+        KeyPair keyPair = null;
+        try{
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(2048);
+            keyPair = kpg.generateKeyPair();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return keyPair;
+
+    }
+
+    public static String encryptRSAToString(String Text,String publicKey) {
+        String encryptedBase64 = "";
+        try {
+            KeyFactory keyFac = KeyFactory.getInstance("RSA");
+            KeySpec keySpec = new X509EncodedKeySpec(Base64.decode(publicKey.trim().getBytes(), Base64.DEFAULT));
+            Key key = keyFac.generatePublic(keySpec);
+
+            // get an RSA cipher object and print the provider
+            final Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING");
+            // encrypt the plain text using the public key
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+
+            byte[] encryptedBytes = cipher.doFinal(Text.getBytes("UTF-8"));
+            encryptedBase64 = new String(Base64.encode(encryptedBytes, Base64.DEFAULT));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return encryptedBase64.replaceAll("(\\r|\\n)", "");//new line or carriage return replace kar and send kr
+    }
+
+    public static String decryptRSAToString(String encryptedBase64, String privateKey) {
+        String decryptedString = "";
+        try {
+            KeyFactory keyFac = KeyFactory.getInstance("RSA");
+            KeySpec keySpec = new PKCS8EncodedKeySpec(Base64.decode(privateKey.trim().getBytes(), Base64.DEFAULT));
+            Key key = keyFac.generatePrivate(keySpec);
+
+            // get an RSA cipher object and print the provider
+            final Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWITHSHA-256ANDMGF1PADDING");
+            // encrypt the plain text using the public key
+            cipher.init(Cipher.DECRYPT_MODE, key);
+
+            byte[] encryptedBytes = Base64.decode(encryptedBase64, Base64.DEFAULT);
+            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+            decryptedString = new String(decryptedBytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return decryptedString;
+    }
+
 }
